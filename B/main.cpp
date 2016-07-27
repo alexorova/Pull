913dcede38cfd71e2af1eccb574ab27466c557c5
Aleksandra <alexorova310198@icloud.com>
2016-07-27 16:37:58 +0300

    Немного другое решение, но тоже норм

--- B/main.cpp
+++ B/main.cpp
@@ -2,9 +2,8 @@
 
 int main()
 {
-    int number;
-    std::cin >> number;
-    std::cout << "The next number for the number " << number << " is " << number + 1 << std::endl;
-    std::cout << "The previous number for the number " << number << " is " << number - 1 << std::endl;
-    return 0;
+   long int a;
+   std::cin >> a;
+   std::cout << "The next number for the number " << a << " is " << a + 1 << std::endl;
+   std::cout << "The previous number for the number" << a << " is " << a - 1 << std::endl;
 }
