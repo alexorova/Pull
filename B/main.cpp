7c522d3248482d4fe9256706c71e44d7e8b7f44a
Aleksandra <alexorova310198@icloud.com>
2016-07-27 16:41:09 +0300

    B completed - частичное решение

--- B/main.cpp
+++ B/main.cpp
@@ -3,7 +3,8 @@
 int main()
 {
    long int a;
+   std::cout << "input number: \n";
    std::cin >> a;
    std::cout << "The next number for the number " << a << " is " << a + 1 << std::endl;
-   std::cout << "The previous number for the number" << a << " is " << a - 1 << std::endl;
+   std::cout << "The previous number for the number " << a << " is " << a - 1 << std::endl;
 }
