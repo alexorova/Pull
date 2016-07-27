f2279fe22c85b97b0122bd9bda10e01250891857
Aleksandra <alexorova310198@icloud.com>
2016-07-27 16:11:40 +0300

    Я считаю, что все норм, но informatics.mccme пишет, что частичное решение :(

---
+++ B/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int number;
+    std::cin >> number;
+    std::cout << "The next number for the number " << number << " is " << number + 1 << std::endl;
+    std::cout << "The previous number for the number " << number << " is " << number - 1 << std::endl;
+    return 0;
+}
